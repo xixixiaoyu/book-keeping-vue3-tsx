31aee1672f08a9b6f59e453b314612687dc31326
云牧 <yunmucoder@163.com>
2022-07-30 21:58:12 +0800

    配置TSX、Vue-Router

---
+++ src/views/Bar.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export const Bar = defineComponent({
+  setup: (props, context) => {
+    return () => <div>Bar</div>;
+  },
+});
---
+++ src/views/Foo.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export const Foo = defineComponent({
+  setup: (props, context) => {
+    return () => <div>Foo</div>;
+  },
+});
