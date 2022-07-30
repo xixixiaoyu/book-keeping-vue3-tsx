ad0ce70fdf9182298e86395b78a3959edd6e3b40
云牧 <yunmucoder@163.com>
2022-07-31 00:05:17 +0800

    feature: CSS初始化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,19 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from "./Welcome.module.scss";
+import logo from "../assets/icons/mangosteen.svg";
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <RouterView />
+      <div class={s.wrapper}>
+        <header>
+          <img src={logo} />
+          <h1>山橘记账</h1>
+        </header>
+        <main>
+          <RouterView />
+        </main>
       </div>
     );
   },
