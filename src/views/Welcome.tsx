948d108da25e9d2060e71d3745a1351eac88b04d
yunumu <1416900346@qq.com>
2023-04-02 21:16:16 +0800

    滑动切换路由

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,18 +1,14 @@
-import { defineComponent, Transition, VNode, ref, watchEffect } from 'vue'
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue'
 import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router'
-import s from './welcome.module.scss'
 import { useSwipe } from '../hooks/useSwipe'
-
+import s from './Welcome.module.scss'
 export const Welcome = defineComponent({
 	setup: (props, context) => {
-		const main = ref<HTMLElement | null>(null)
-
-		const { direction, swiping } = useSwipe(main)
-
-		watchEffect(() => {
-			console.log(swiping.value, direction.value)
-		})
-
+		const main = ref<HTMLElement>()
+		// const { direction, swiping } = useSwipe(main)
+		// watchEffect(() => {
+		//   console.log(swiping.value, direction.value)
+		// })
 		return () => (
 			<div class={s.wrapper}>
 				<header>
