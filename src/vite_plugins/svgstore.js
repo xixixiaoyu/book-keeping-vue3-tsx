de35a7058e0ea66609ef534f9c7fdf5fdc4b45d8
yunumu <1416900346@qq.com>
2023-04-02 17:31:56 +0800

    优化 svg sprite

--- src/vite_plugins/svgstore.js
+++ src/vite_plugins/svgstore.js
@@ -36,14 +36,8 @@ export const svgstore = (options = {}) => {
 				})
 				return `const div = document.createElement('div')
 div.innerHTML = \`${code}\`
+div.style.display = 'none'
 const svg = div.getElementsByTagName('svg')[0]
-if (svg) {
-  svg.style.position = 'absolute'
-  svg.style.width = 0
-  svg.style.height = 0
-  svg.style.overflow = 'hidden'
-  svg.setAttribute("aria-hidden", "true")
-}
 // listen dom ready event
 document.addEventListener('DOMContentLoaded', () => {
   if (document.body.firstChild) {
