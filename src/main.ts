0fca4d91fe0882d2ce726458b976c6ce774204ea
yunumu <1416900346@qq.com>
2023-04-02 17:21:23 +0800

    使用 SVG Sprites

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from "vue";
-import { App } from "./App";
-import { routes } from "./config/routes";
-import { createRouter } from "vue-router";
-import { history } from "./shared/history";
+import { createApp } from 'vue'
+import { App } from './App'
+import { routes } from './config/routes'
+import { createRouter } from 'vue-router'
+import { history } from './shared/history'
+import '@svgstore'
 
 const router = createRouter({
-  history,
-  routes,
-});
+	history,
+	routes,
+})
 
-const app = createApp(App);
-app.use(router);
-app.mount("#app");
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
