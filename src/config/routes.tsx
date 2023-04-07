d7f4ebae2ba91d4cceae7895044dfe98e34191fa
yunumu <1416900346@qq.com>
2023-04-07 23:02:00 +0800

    wip: 创建 TagEdit.tsx

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -43,7 +43,7 @@ export const routes: RouteRecordRaw[] = [
 		component: TagPage,
 		children: [
 			{ path: 'create', component: TagCreate },
-			{ path: ':id', component: TagEdit },
+			{ path: ':id/edit', component: TagEdit },
 		],
 	},
 ]
