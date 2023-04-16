f693474d0171f02d28945bd07ff14a963e69a817
yunumu <1416900346@qq.com>
2023-04-16 10:43:47 +0800

    创建统计图表页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -16,6 +16,7 @@ import { TagCreate } from '../components/tag/TagCreate'
 import { TagEdit } from '../components/tag/TagEdit'
 import { TagPage } from '../views/TagPage'
 import { SignInPage } from '../views/SignInPage'
+import { StatisticsPage } from '../views/StatisticsPage'
 
 export const routes: RouteRecordRaw[] = [
 	{ path: '/', redirect: '/welcome' },
@@ -50,5 +51,9 @@ export const routes: RouteRecordRaw[] = [
 	{
 		path: '/sign_in',
 		component: SignInPage,
+	},
+	{
+		path: '/statistics',
+		component: StatisticsPage,
 	},
 ]
