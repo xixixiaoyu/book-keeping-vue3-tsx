a5b492c34ade5faf3643f99fc59ef9b1cbe7f9b5
yunumu <1416900346@qq.com>
2023-04-29 23:17:31 +0800

    收集了四个字段 kind、tag_id、happen_at 和 amount

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -5,13 +5,10 @@ import s from './InputPad.module.scss'
 import { DatetimePicker, NumberKeyboard, Popup } from 'vant'
 export const InputPad = defineComponent({
 	props: {
-		name: {
-			type: String as PropType<string>,
-		},
+		happenAt: String,
+		amount: Number,
 	},
 	setup: (props, context) => {
-		const now = new Date()
-		const refDate = ref<Date>(now)
 		const appendText = (n: number | string) => {
 			const nString = n.toString()
 			const dotIndex = refAmount.value.indexOf('.')
@@ -114,26 +111,29 @@ export const InputPad = defineComponent({
 					refAmount.value = '0'
 				},
 			},
-			{ text: '提交', onClick: () => {} },
+			{
+				text: '提交',
+				onClick: () => context.emit('update:amount', parseFloat(refAmount.value) * 100),
+			},
 		]
 		const refDatePickerVisible = ref(false)
 		const showDatePicker = () => (refDatePickerVisible.value = true)
 		const hideDatePicker = () => (refDatePickerVisible.value = false)
 		const setDate = (date: Date) => {
-			refDate.value = date
+			context.emit('update:happenAt', date.toISOString())
 			hideDatePicker()
 		}
-		const refAmount = ref('0')
+		const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0')
 		return () => (
 			<>
 				<div class={s.dateAndAmount}>
 					<span class={s.date}>
 						<Icon name="date" class={s.icon} />
 						<span>
-							<span onClick={showDatePicker}>{new Time(refDate.value).format()}</span>
+							<span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
 							<Popup position="bottom" v-model:show={refDatePickerVisible.value}>
 								<DatetimePicker
-									value={refDate.value}
+									value={props.happenAt}
 									type="date"
 									title="选择年月日"
 									onConfirm={setDate}
