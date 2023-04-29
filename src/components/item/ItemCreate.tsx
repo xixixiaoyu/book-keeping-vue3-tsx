0f70f97dae6f3ca2fb0e4f74a4b1df2d35c31bae
yunumu <1416900346@qq.com>
2023-04-29 23:24:25 +0800

    封装后退按钮

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -3,6 +3,7 @@ import { Dialog } from 'vant'
 import { defineComponent, onMounted, PropType, reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { MainLayout } from '../../layouts/MainLayout'
+import { BackIcon } from '../../shared/BackIcon'
 import { Button } from '../../shared/Button'
 import { http } from '../../shared/Http'
 import { Icon } from '../../shared/Icon'
@@ -36,7 +37,9 @@ export const ItemCreate = defineComponent({
 		}
 		const onSubmit = async () => {
 			await http
-				.post<Resource<Item>>('/items', formData, { params: { _mock: 'itemCreate' } })
+				.post<Resource<Item>>('/items', formData, {
+					params: { _mock: 'itemCreate' },
+				})
 				.catch(onError)
 			router.push('/items')
 		}
@@ -44,7 +47,7 @@ export const ItemCreate = defineComponent({
 			<MainLayout class={s.layout}>
 				{{
 					title: () => '记一笔',
-					icon: () => <Icon name="left" class={s.navIcon} />,
+					icon: () => <BackIcon />,
 					default: () => (
 						<>
 							<div class={s.wrapper}>
--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, reactive } from 'vue'
 import { MainLayout } from '../../layouts/MainLayout'
+import { BackIcon } from '../../shared/BackIcon'
 import { Button } from '../../shared/Button'
 import { EmojiSelect } from '../../shared/EmojiSelect'
 import { Icon } from '../../shared/Icon'
@@ -30,7 +31,7 @@ export const TagCreate = defineComponent({
 			<MainLayout>
 				{{
 					title: () => '新建标签',
-					icon: () => <Icon name="left" onClick={() => {}} />,
+					icon: () => <BackIcon />,
 					default: () => <TagForm />,
 				}}
 			</MainLayout>
