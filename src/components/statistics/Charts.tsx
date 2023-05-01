86d81b4232b0671adb5bcc573fb04f2d5a4025fc
yunumu <1416900346@qq.com>
2023-05-01 23:04:04 +0800

    切换 kind 时重新请求数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue'
+import { computed, defineComponent, onMounted, PropType, ref, watch } from 'vue'
 import { FormItem } from '../../shared/Form'
 import s from './Charts.module.scss'
 import { LineChart } from './LineChart'
@@ -42,7 +42,7 @@ export const Charts = defineComponent({
 			})
 		})
 
-		onMounted(async () => {
+		const fetchData1 = async () => {
 			const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
 				happen_after: props.startDate,
 				happen_before: props.endDate,
@@ -51,7 +51,9 @@ export const Charts = defineComponent({
 				_mock: 'itemSummary',
 			})
 			data1.value = response.data.groups
-		})
+		}
+		onMounted(fetchData1)
+		watch(() => kind.value, fetchData1)
 
 		const data2 = ref<Data2>([])
 		const betterData2 = computed<{ name: string; value: number }[]>(() =>
@@ -69,7 +71,7 @@ export const Charts = defineComponent({
 			}))
 		})
 
-		onMounted(async () => {
+		const fetchData2 = async () => {
 			const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
 				happen_after: props.startDate,
 				happen_before: props.endDate,
@@ -78,7 +80,9 @@ export const Charts = defineComponent({
 				_mock: 'itemSummary',
 			})
 			data2.value = response.data.groups
-		})
+		}
+		onMounted(fetchData2)
+		watch(() => kind.value, fetchData2)
 
 		return () => (
 			<div class={s.wrapper}>
